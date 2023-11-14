26ad22a2cc3f0b75190dc4cea4349b1418ec4446
Tony Thompson <150488583+TonyThompsonPHX@users.noreply.github.com>
2023-11-14 13:03:47 -0700

    Update streamlit_app.py
    adjusted title slightly

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My parents new healthy diner.')
+streamlit.title('My parents new healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
