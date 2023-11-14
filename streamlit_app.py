711d580499f10abf5553a83f23745ecf952543cc
Tony Thompson <150488583+TonyThompsonPHX@users.noreply.github.com>
2023-11-14 12:59:19 -0700

    Update streamlit_app.py
    moved fruit index statement up a few lines to be right under the read_csv line

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-import streamlit, pandas as pd
+import streamlit
 
 streamlit.title('My parents new healthy diner.')
 
@@ -10,11 +10,11 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-# The below line is the link to the text file in the S3 bucket
+import pandas as pd # The below line is the link to the text file in the S3 bucket
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
-my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.dataframe(my_fruit_list)
